b8de7b067d852febbfe270e451332fb06060f9de
Anus Ali Khan <anusalikhan1405@gmail.com>
2023-08-21 20:32:56 +0500

    adding cart items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
 import { CartIcon } from "./icons";
+import CartContainer from "./components/CartContainer";
 
 function App() {
   return (
     <main>
       <Navbar />
+      <CartContainer />
     </main>
   );
 }
